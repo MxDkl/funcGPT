07a663978f1acb9b69601f8d215263b7e693e792
Max Dekel <max@dekelnet.com>
2023-04-12 09:26:27 -0400

    Fixed typo

--- funcGPT.py
+++ funcGPT.py
@@ -18,7 +18,7 @@ def simplest_type(s):
 # A decorator that uses GPT-3.5 to 'calculate' the output of a function
 def funcGPT(function):
     def wrapper(*args, **kwargs):
-        system_prompt = "You are a helpful assistant. Given the name of a python function and its arguments, you will return what you think is the expected output of the function. You will not explain your response. You will NEVER preface your response with anything. NDo notEVER end your response with a period."
+        system_prompt = "You are a helpful assistant. Given the name of a python function and its arguments, you will return what you think is the expected output of the function. You will not explain your response. You will NEVER preface your response with anything. NEVER end your response with a period."
         prompt = "What is the expected output of the following python function: " + function.__name__ + ", with the following arguments: " + str(args) + " and " + str(kwargs) + "?"
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
