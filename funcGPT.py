4d3147e3d4b64fa7c5feea3aaae8994e1606a644
Max Dekel <max@dekelnet.com>
2023-12-28 09:09:16 -0500

    gpt-4 turbo

--- funcGPT.py
+++ funcGPT.py
@@ -1,10 +1,13 @@
 import os
 from dotenv import load_dotenv
-import openai
+from openai import OpenAI
 from ast import literal_eval
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+OpenAI.api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI()
+
+MODEL = "gpt-4-1106-preview"
 
 
 # A helper function that tries to convert a string to a python type
@@ -15,36 +18,18 @@ def simplest_type(s):
         return s
 
 
-# A decorator that uses GPT-3.5 to 'calculate' the output of a function
+# A decorator that uses gpt to 'calculate' the output of a function
 def funcGPT(function):
     def wrapper(*args, **kwargs):
-        system_prompt = "You are a helpful assistant. Given the name of a python function and its arguments, you will return what you think is the expected output of the function. You will not explain your response. You will NEVER preface your response with anything. NEVER end your response with a period."
+        system_prompt = "You are a helpful assistant. Given the name of a python function and its arguments, you will return what you think is the expected output of the function. You will not explain your response. You will NEVER preface your response with anything. NEVER end your response with a period. Even if you think the function name is ambiguous."
         prompt = "What is the expected output of the following python function: " + function.__name__ + ", with the following arguments: " + str(args) + " and " + str(kwargs) + "?"
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+        response = client.chat.completions.create(
+            model=MODEL,
             messages=[
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": prompt}
             ]
         )
-        response = response["choices"][0]["message"]["content"]
+        response = response.choices[0].message.content
         return simplest_type(response)
-    return wrapper
-
-
-# a decorator that uses GPT-3.5 to print what it thinks the output of a function is
-def printGPT(function):
-    def wrapper(*args, **kwargs):
-        system_prompt = "You are a helpful assistant. Given the name of a python function and its arguments, you will return what you think is the expected output of the function."
-        prompt = "What is the expected output of the following python function: " + function.__name__ + ", with the following arguments: " + str(args) + " and " + str(kwargs) + "?"
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": prompt}
-            ]
-        )
-        response = response["choices"][0]["message"]["content"]
-        print(response)
-        return function(*args, **kwargs)
     return wrapper
--- tests.py
+++ tests.py
@@ -9,8 +9,14 @@ def sort(a : list):
 def nth_fibonacci(n : int):
     return
 
+# a random complex function
+@funcGPT.funcGPT
+def sentiment_analisys_minus_1_to_1(sentence : str):
+    return
+
 if __name__ == "__main__":
     sorted = sort(['mango', 'cabbage', 'date', 'apple', 'banana'])
-    fib = nth_fibonacci(5)
-    print(sorted[0], fib)
-
+    fib = nth_fibonacci(30)
+    num1 = sentiment_analisys_minus_1_to_1("I liked the movie")
+    num2 = sentiment_analisys_minus_1_to_1("I LOVED the movie!")
+    print(sorted, fib, num1, num2)
